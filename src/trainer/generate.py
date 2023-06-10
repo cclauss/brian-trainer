f4e4ecd3700ad92f739b254d89c5590e7f46957d
PaoloRollo <paolo.rollo1997@gmail.com>
2023-06-10 14:47:36 +0200

    feat: removed serverless. useless.

--- src/trainer/generate.py
+++ src/trainer/generate.py
@@ -1,9 +1,41 @@
 import os
 import json
 from dotenv import load_dotenv
-from queries import execute_query, SUSHISWAP_POOLS_GNOSIS_QUERY
+from queries import execute_query, SUSHISWAP_POOLS_GNOSIS_QUERY, UNISWAP_V3_POOLS_QUERY
 
 load_dotenv()
+
+def generate_ethereum_token_mapping():
+    try:
+        # Create the basic prompts
+        token_map = {}
+        # Get the top pools by cumulative volume
+        uniswap_result = execute_query(os.environ['UNISWAP_V3_THEGRAPH_URL'], UNISWAP_V3_POOLS_QUERY)
+        for pool in uniswap_result['liquidityPools']:
+            for input_token in pool["inputTokens"]:
+                if input_token["symbol"] not in token_map:
+                    token_map[input_token["symbol"]] = input_token["id"]
+        # Generate the file that will be used to train the model
+        with open('prompts/ethereum_tokens.json', 'w') as prompts_file:
+            json.dump(token_map, prompts_file)
+    except Exception as e:
+        print('an error has occurred while generating ethereum tokens mapping: %s' % (str(e), ))
+
+def generate_gnosis_token_mapping():
+    try:
+        # Create the basic prompts
+        token_map = {}
+        # Get the top pools by cumulative volume
+        sushiswap_result = execute_query(os.environ['SUSHISWAP_GNOSIS_THEGRAPH_URL'], SUSHISWAP_POOLS_GNOSIS_QUERY)
+        for pool in sushiswap_result['liquidityPools']:
+            for input_token in pool["inputTokens"]:
+                if input_token["symbol"] not in token_map:
+                    token_map[input_token["symbol"]] = input_token["id"]
+        # Generate the file that will be used to train the model
+        with open('prompts/gnosis_tokens.json', 'w') as prompts_file:
+            json.dump(token_map, prompts_file)
+    except Exception as e:
+        print('an error has occurred while generating gnosis tokens mapping: %s' % (str(e), ))
 
 def generate_gnosis_tokens_approval():
     try:
@@ -67,6 +99,8 @@ def generate_gnosis_tokens_balance():
     except Exception as e:
         print('an error has occurred while generating gnosis tokens balance prompts: %s' % (str(e), ))
 
+generate_ethereum_token_mapping()
+generate_gnosis_token_mapping()
 generate_gnosis_tokens_approval()
 generate_gnosis_tokens_transfer()
 generate_gnosis_tokens_balance()
